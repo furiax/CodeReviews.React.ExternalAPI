453db50e40bc2e1342ca1679670e8f3f0a9ac383
furiax <carlmalfliet@proximus.be>
2025-01-22 20:49:07 +0100

    Start project by creating the context file

---
+++ ExternalAPI.Furiax/FormulaOneServer/Data/FormulaDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class FormulaDbContext : DbContext
+{
+    public FormulaDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+}
---
+++ ExternalAPI.Furiax/FormulaOneServer/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<FormulaDbContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
